6cb9a9c8e783b6aba3e8587428024c8507b83613
thorsteneckstein <thorsten_eckstein@web.de>
2024-01-27 18:06:22 +0100

    added (exam) questions (wip)

--- module-classes/src/test/java/de/dhbw/questions/ExamQuestions.java
+++ module-classes/src/test/java/de/dhbw/questions/ExamQuestions.java
@@ -1,40 +1,75 @@
 package de.dhbw.questions;
 
+import de.dhbw.commons.Answer;
+import de.dhbw.solution.ExamAnswers;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static de.dhbw.commons.Answers.*;
+import static de.dhbw.commons.Answers.butMaybeWrong;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @Disabled
-@SuppressWarnings({"NewClassNamingConvention", "ConstantValue"})
+@SuppressWarnings({"All"})
 @DisplayName(
-        "Questions for module data types"
+        "Questions for module classes, objects, instances"
 )
 public class ExamQuestions {
 
-    //private final ExamAnswers correctAnswers = new ExamAnswers();
+    private final ExamAnswers correctAnswers = new ExamAnswers();
 
     @DisplayName(
             """
-            Q1:...
+            Q1: Consider the following class definition:
+            
+            public class Test extends Base {
+              public Test(int j) {}
+              public Test(int j, int k) { super(j, k); }
+            }
+            
+            Which of the following are legitimate calls to construct instances of the Test class?
+            
+            A. Test t = new Test();
+            B. Test t = new Test(1);
+            C. Test t = new Test(1, 2);
+            D. Test t = new Test(1, 2, 3);
+            E. Test t = new Base();
             """
     )
-    @Test
-    public void question1() {
-        // question without a parameter as answer
+    @ValueSource(strings = {"?"})
+    @ParameterizedTest
+    public void question1(String studentAnswer) {
+        // given
+        Answer correctAnswer = correctAnswers.of(Q1);
+
+        // when
+        boolean isCorrect = correctAnswer.getLetters().contains(studentAnswer);
+
+        // then
+        assertTrue(isCorrect, butMaybeWrong(Q1, studentAnswer));
     }
 
     @DisplayName(
             """
-            Q2:...
+            Q2: Liste die drei Schritte zur Erzeugung eines Objektes für eine Klasse auf?
             """
     )
-    // Please give your answer here:
-    @ValueSource(strings = {"?"})
-    @ParameterizedTest
-    public void question2(String studentAnswer) {
-        // question with a letter as answer
+    @Test
+    public void question2() {
+        // given
+        Answer commonExplanation = correctAnswers.of(Q2);
+
+        // when
+        final String studentExplanation =
+                """
+                ... your answer here ...
+                """;
+
+        // then
+        System.out.println(commonExplanation.face(studentExplanation));
     }
 
 }
--- module-classes/src/test/java/de/dhbw/solution/ExamAnswers.java
+++ module-classes/src/test/java/de/dhbw/solution/ExamAnswers.java
@@ -3,10 +3,43 @@ package de.dhbw.solution;
 import de.dhbw.commons.Answer;
 import de.dhbw.commons.Answers;
 
+@SuppressWarnings("ALL")
 public class ExamAnswers extends Answers {
 
     static {
-        answers.put(Q1, Answer.from("<correct-letter-here>"));
+        answers.put(Q1, Answer.fromMany("B","C"));
+        answers.put(Q2, Answer.fromExplanation("""
+                Ein Objekt wird zuerst deklariert, dann instanziiert, dann initialisiert.
+                
+                1. Deklaration    : Die Variablendeklaration mit einem Variablennamen und einem Objekttyp                                                                                                                                
+                2. Instanziierung : Das Schlüsselwort „new“ wird zum Erstellen des Objekts verwendet                                                                                                                             
+                3. Initialization : Auf das Schlüsselwort „new“ folgt ein Aufruf eines Konstruktors. 
+                                    Dieser Aufruf initialisiert das neue Objekt.
+                """)
+        );
+    }
+
+    //
+    // Demonstrations for questions
+    // May not compile, therefore some elements commented
+    //
+
+    private class DemoForQ1 {
+
+        //Test t1 = new Test();
+        Test t2 = new Test(1);
+        Test t3 = new Test(1, 2);
+        //Test t4 = new Test(1, 2, 3);
+        //Test t6 = new Base();
+
+        public class Base {
+            public Base(){}
+            public Base(int j, int k){}
+        }
+        public class Test extends Base {
+            public Test(int j) {}
+            public Test(int j, int k) { super(j, k); }
+        }
     }
 
 }

234667b74558f95ca83d0849b71b4304d1bdd9d6
thorsteneckstein <thorsten_eckstein@web.de>
2024-02-09 15:15:27 +0100

    fixed typos, added exercise

--- module-statics/src/test/java/de/dhbw/exercise/StaticsExerciseTest.java
+++ module-statics/src/test/java/de/dhbw/exercise/StaticsExerciseTest.java
@@ -6,7 +6,12 @@ import org.junit.jupiter.api.Test;
 public class StaticsExerciseTest {
 
     @Test
-    @DisplayName("Übung 1: ")
+    @DisplayName("""
+            Übung 1: Erzeuge ...
+            a) eine Klasse Car mit einem Feld 'type' (String) und einem zugehörigen Konstruktor
+            b) eine Klasse CarFactory mit einer statischen Methode mit dem Parameter 'type', die neue Car-Instanzen zurückgibt
+            c) nutze diesen Test hier, in dem die Factory benutzt wird und den/die Car-Type(s) testet
+            """)
     public void exercise1() {
         // given
 

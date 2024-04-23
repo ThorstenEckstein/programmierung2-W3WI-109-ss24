0e60ba9389c2f4bac6ca841aed218c99a5e92857
thorsteneckstein <thorsten_eckstein@web.de>
2024-04-23 12:10:23 +0200

    password als beispiel

--- exam-preparation/src/test/java/de/dhbw/exam/ProgrammingTests.java
+++ exam-preparation/src/test/java/de/dhbw/exam/ProgrammingTests.java
@@ -3,6 +3,12 @@ package de.dhbw.exam;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ProgrammingTests {
 
     @Test
@@ -16,9 +22,17 @@ public class ProgrammingTests {
     @Test
     @DisplayName("Gleich ist nicht gleich gleich")
     public void canImplementEqualsIsNotEquals() {
+        // given
+        Password p1 = new Password("askdujhasjd6we5463");
+        Password p2 = new Password("askdujhasjd6we5463");
 
-        // implement your test code here
+        // when
+        boolean areEqual = p1.equals(p2);
 
+        // then
+        assertTrue(areEqual);
+
+        List<Object> objects = new LinkedList<>();
     }
 
 }
